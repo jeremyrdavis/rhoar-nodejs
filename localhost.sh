ba4f0094dd6cd77c7b5b8ab3c68e41815053d91f
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2018-06-01 07:47:01 -0400

    noun service boots on its' own

--- localhost.sh
+++ localhost.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-cd adjective
+#cd adjective
 # npm install
-PORT=8081 node . &
-cd ../noun
+#PORT=8081 node . &
+cd noun
 # npm install
-PORT=8082 node . &
-cd ../insult
+#PORT=8082 node . &
+#cd ../insult
 # npm install
-ADJECTIVE_SERVICE=localhost ADJECTIVE_SERVICE_PORT=8081 NOUN_SERVICE=localhost NOUN_SERVICE_PORT=8082 node .
+#ADJECTIVE_SERVICE=localhost ADJECTIVE_SERVICE_PORT=8081 NOUN_SERVICE=localhost NOUN_SERVICE_PORT=8082 node .
+NOUN_SERVICE=localhost NOUN_SERVICE_PORT=8082 node .
